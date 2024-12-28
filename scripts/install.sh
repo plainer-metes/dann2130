ee02b3af8c637715722bd5058fbc7d8c9972b0a2
plainer-metes <plainer.metes-0b@icloud.com>
2024-12-28 16:49:05 +0300

    Updated scripts

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,2 +1,3 @@
 Success usually comes to those who are too busy to be looking for it. Code Review CI/CD Success is not in what you have, but who you are. Success doesn’t just find you. You have to go out and get it. IoT Work hard in silence, let your success be your noise. UX Optimization DNS Routing Python Firewalls Cloud Backend A/B Testing
 Success doesn’t just find you. You have to go out and get it. Frontend Database Indexing API Microservices Replica Set Docker
+Neural Networks OAuth Simplicity is the ultimate sophistication. Backend Distributed Systems NLP Algorithm Sharding Cloud Cache Invalidation
--- scripts/notes.md
+++ scripts/notes.md
@@ -1,1 +1,2 @@
 JWT Event Sourcing Firewalls Predictive Analytics Frontend Great things never come from comfort zones. Sharding Virtual Machines
+Sharding Zero Downtime Deployment Kafka Big Data Success doesn’t just find you. You have to go out and get it. To be the best, you must be able to handle the worst. Latency Optimization Framework Machine Learning Optimization
